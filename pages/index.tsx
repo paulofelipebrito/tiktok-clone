20a3ed096e0020de9a7be1bc738424f178e524b1
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-13 06:32:25 -0300

    feat: home page (Video view)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import axios from 'axios';
 
-// interface IProps {
-//   videos: Video[];
-// }
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
 
-const Home = () => {
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
   return (
     <div className='flex flex-col gap-10 videos h-full'>
-      hello
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
     </div>
   );
 };
 
 export default Home;
 
-// export const getServerSideProps = async ({
-//   query: { topic },
-// }: {
-//   query: { topic: string };
-// }) => {
-//   let response = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
 
-//   if(topic) {
-//     response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
-//   }
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
   
-//   return {
-//     props: { videos: response.data },
-//   };
-// };
+  return {
+    props: { videos: response.data },
+  };
+};
